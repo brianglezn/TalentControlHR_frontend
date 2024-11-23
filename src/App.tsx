614f75eb22188080e4a0f7215d2612da4fa1d30e
Brian González Novoa <119687340+brianglezn@users.noreply.github.com>
2024-11-23 13:20:01 +0100

    Configuramos rutas relativas para los import

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 import { Routes, Route } from 'react-router-dom';
 
-import Dashboard from "./pages/dashboard/Dashboard";
-import Home from "./pages/landing/Home";
+import Dashboard from '@pages/dashboard/Dashboard';
+import Home from '@pages/landing/Home';
 
 export default function App() {
-
   return (
     <>
       <Routes>
-        <Route path='/' element={<Home />}></Route>
-        <Route path='/dashboard' element={<Dashboard />}></Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </>
-  )
+  );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   css: {
@@ -11,4 +11,14 @@ export default defineConfig({
       },
     },
   },
-})
+  resolve: {
+    alias: {
+      '@api': path.resolve(__dirname, './src/api'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@context': path.resolve(__dirname, './src/context'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+    },
+  },
+});
