ce8973f8bf0110b5b88252b444167604a91c920e
Brian González Novoa <119687340+brianglezn@users.noreply.github.com>
2024-11-24 18:24:06 +0100

    Update types.ts

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,13 +1,19 @@
+// User types
 export interface User {
+    userId: string;
     username: string;
     name: string;
-    team: string;
     surnames: string;
     email: string;
+    password: string;
     role: string;
+    company: Company;
+    team: CompanyTeam;
 }
 
+// Company types
 export interface Company {
+    companyId: string;
     name: string;
     description: string;
     industry: string;
@@ -16,7 +22,34 @@ export interface Company {
 }
 
 export interface CompanyTeam {
+    teamId: string;
     name: string;
     description: string;
     color: string;
+    company: Company;
+}
+
+// TimeOff types
+export interface Attendance {
+    attendanceId: string;
+    user: User;
+    date: Date;
+    endDate?: Date;
+    type: AttendanceType;
+    status: AttendanceStatus;
+    reason?: string;
+}
+
+export type AttendanceType = 'Vacations' | 'Sickness' | 'Sickness of a family member' | 'Maternity / Paternity' | 'Other';
+export type AttendanceStatus = 'Pending' | 'Approved' | 'Denied';
+
+// Shifts types
+export interface Shift {
+    shiftId: string;
+    name: string;
+    user: User;
+    team: CompanyTeam;
+    startTime: Date;
+    endTime: Date;
+    notes?: string;
 }
